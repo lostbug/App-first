45c678a5981b5518a9b1b7b6ed3add766bc18a2f
rockan007 <rockan007@gmail.com>
2017-09-19 21:37:49 +0800

    ActionBar标题居中

--- app/src/main/java/com/lostbug/app_first/MainActivity.java
+++ app/src/main/java/com/lostbug/app_first/MainActivity.java
@@ -7,6 +7,7 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
 
 import com.lostbug.app_first.adapters.MainAdapter;
 import com.lostbug.app_first.base.BaseActivity;
@@ -18,10 +19,12 @@ import com.lostbug.app_first.orders.OrdersFragment;
 
 import java.util.ArrayList;
 
-public class MainActivity extends BaseActivity implements ViewPager.OnPageChangeListener,BottomNavigationView.OnNavigationItemSelectedListener {
+public class MainActivity extends BaseActivity implements ViewPager.OnPageChangeListener, BottomNavigationView.OnNavigationItemSelectedListener {
     BottomNavigationView navigation;
     ViewPager mViewPage;
-    final static String TAG="MainActivity";
+    private int mCurrentPosition = 0;
+    final static String TAG = "MainActivity";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,16 +32,33 @@ public class MainActivity extends BaseActivity implements ViewPager.OnPageChange
         navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setItemIconTintList(null);
         navigation.setOnNavigationItemSelectedListener(this);
-        ArrayList<BaseFragment> fragments=new ArrayList<>();
+        ArrayList<BaseFragment> fragments = new ArrayList<>();
         fragments.add(new HomeFragment());
         fragments.add(new ClientFragment());
         fragments.add(new OrdersFragment());
         fragments.add(new MineFragment());
-         mViewPage=(ViewPager)findViewById(R.id.main_viewPager);
-        MainAdapter mainAdapter=new MainAdapter(getSupportFragmentManager(),fragments);
+        mViewPage = (ViewPager) findViewById(R.id.main_viewPager);
+        MainAdapter mainAdapter = new MainAdapter(getSupportFragmentManager(), fragments);
         mViewPage.setAdapter(mainAdapter);
         mViewPage.addOnPageChangeListener(this);
     }
+
+    @Override
+    public void titleRightIconClick() {
+        switch (mCurrentPosition) {
+            case 0://首页
+                break;
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            default:
+                break;
+        }
+    }
+    
 
     @Override
     public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
@@ -47,23 +67,24 @@ public class MainActivity extends BaseActivity implements ViewPager.OnPageChange
 
     @Override
     public void onPageSelected(int position) {
-        Log.e(TAG,"当前界面"+position);
+        Log.e(TAG, "当前界面" + position);
         int id;
-        switch (position){
+        mCurrentPosition = position;
+        switch (position) {
             case 0:
-                id=R.id.navigation_home;
+                id = R.id.navigation_home;
                 break;
             case 1:
-                id=R.id.navigation_client;
+                id = R.id.navigation_client;
                 break;
             case 2:
-                id=R.id.navigation_orders;
+                id = R.id.navigation_orders;
                 break;
             case 3:
-                id=R.id.navigation_mine;
+                id = R.id.navigation_mine;
                 break;
             default:
-                id=R.id.navigation_home;
+                id = R.id.navigation_home;
                 break;
         }
         navigation.setSelectedItemId(id);
@@ -76,23 +97,23 @@ public class MainActivity extends BaseActivity implements ViewPager.OnPageChange
 
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-        Log.d(TAG,"当前id:"+item.getItemId());
+        Log.d(TAG, "当前id:" + item.getItemId());
         switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mViewPage.setCurrentItem(0);
-                    return true;
-                case R.id.navigation_client:
-                    mViewPage.setCurrentItem(1);
-                    return true;
-                case R.id.navigation_orders:
-                    mViewPage.setCurrentItem(2);
-                    return true;
-                case R.id.navigation_mine:
-                    mViewPage.setCurrentItem(3);
-                    return true;
-                default:
-                    return false;
-            }
+            case R.id.navigation_home:
+                mViewPage.setCurrentItem(0);
+                return true;
+            case R.id.navigation_client:
+                mViewPage.setCurrentItem(1);
+                return true;
+            case R.id.navigation_orders:
+                mViewPage.setCurrentItem(2);
+                return true;
+            case R.id.navigation_mine:
+                mViewPage.setCurrentItem(3);
+                return true;
+            default:
+                return false;
         }
+    }
 
 }
